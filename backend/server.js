6d225853383594e4eb6b0b0fafb02b0c3eaf45d5
J Kenath <kenathkenady@gmail.com>
2026-04-24 19:19:07 +0530

    Fix CORS for production

--- backend/server.js
+++ backend/server.js
@@ -1,3 +1,4 @@
+
 const express = require('express');
 const mongoose = require('mongoose');
 const cors = require('cors');
@@ -9,7 +10,10 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 
 // Middleware
-app.use(cors());
+app.use(cors({
+  origin: '*'
+}));
+
 app.use(express.json());
 
 // Routes
